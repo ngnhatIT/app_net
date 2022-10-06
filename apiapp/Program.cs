2c33307e01e5e218d9e43f88ffca3dd0fe7749cf
nhat nguyen <nguyenkhanhnhat0108@outlook.com>
2022-10-06 13:39:02 +0700

    comment out swagger

--- apiapp/Program.cs
+++ apiapp/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 MongoDbPersistence.Configure();
 builder.Services
